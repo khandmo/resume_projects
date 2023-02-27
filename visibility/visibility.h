5ed9ad9ef979e6612474533fd830728845df67c4
petermoraw <f0044cy@plank.thayer.dartmouth.edu>
2023-02-27 18:51:15 -0500

    Update visibility.h file

--- visibility/visibility.h
+++ visibility/visibility.h
@@ -5,14 +5,56 @@
 #include <stdbool.h>
 #include "../grid.h"
 
+
+/****** Point Structure ******/
+/* 
+ * Useful for looping through map grid
+ * 
+ * 
+*/
 typedef struct point{
   int x;
   int y;
 } point_t;
 
 
+/****** isVisible1 ********/
+/*
+ * isVisible1 takes in two points as entry and returns a boolean
+ * saying whether or not the points are visible from each other.
+ * isVisible1 works for points in the first and third quadrants!
+ * In other words, it works when on the grid the line between the
+ * points is shaped like this: / (for (0,0) at top left) 
+ * 
+ */
 bool isVisible1(point_t* start, point_t* end, char* map);
+
+
+
+/****** isVisible2 ********/
+/*
+ * isVisible2 takes in two points as entry and returns a boolean
+ * saying whether or not the points are visible from each other.
+ * isVisible2 works for points in the second and fourth quadrants!
+ * In other words, it works when on the grid the line between the
+ * points is shaped like this: \ (for (0,0) at top left) 
+ * 
+ */
 bool isVisible2(point_t* start, point_t* end, char* map);
+
+
+/****** line_func ********/
+/*
+ * Calculates the y-value of a line given the slope,
+ * starting y-value, and an x coordinate
+ *
+*/
 float line_func(float slope, int x, int y);
+
+/**** point_new ******/
+/*
+ * Creates new point structure with given
+ * x and y values
+*/
 point_t* point_new(int x, int y);
 
