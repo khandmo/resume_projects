1bc2504444d12bf0c6faccd34d75c6a38344daf5
petermoraw <f0044cy@plank.thayer.dartmouth.edu>
2023-02-28 13:27:22 -0500

    Incorporating findVisibility into testing

--- visibility/testvisibility.c
+++ visibility/testvisibility.c
@@ -21,13 +21,16 @@ main(void)
     printf("Null\n");
     exit(1);
   }
+
   char* map = file_readFile(fp);
+  char* map_new = malloc(strlen(map));
+  strcpy(map_new, map);
+
   point_t* start = point_new(2, 6);
-  point_t* end = point_new(11, 9);
+  point_t* end = point_new(12, 6);
   printf("\n %c \n", getCharAtPoint(start, map));
   printf("\n %c \n", getCharAtPoint(end, map));
-  bool yes = isVisible2(start, end, map);
-
+  bool yes = isVisible1(start, end, map);
   printf("Yes is %d\n", yes);
 
   return 0;
